22a07a5d904c2f01fbd110820a0689055a929703
injammahin <injammahin5507@gmail.com>
2026-09-04 00:33:35 +0600

    update

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -8,18 +8,48 @@ use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
     {
+        /*
+        |--------------------------------------------------------------------------
+        | Admin 1
+        |--------------------------------------------------------------------------
+        */
         User::updateOrCreate(
             [
-                'email' => 'admin@gmail.com',
+                'email' => 'mahin@gmail.com',
+            ],
+            [
+                'name' => 'Mahin',
+
+                'phone' => null,
+
+                'password' => Hash::make('Mahin@5507'),
+
+                'role' => 'admin',
+
+                'status' => true,
+            ]
+        );
+
+        /*
+        |--------------------------------------------------------------------------
+        | Admin 2
+        |--------------------------------------------------------------------------
+        */
+        User::updateOrCreate(
+            [
+                'email' => 'admin@midpoint.ng',
             ],
             [
                 'name' => 'Midpoint Administrator',
 
                 'phone' => null,
 
-                'password' => Hash::make('123456'),
+                'password' => Hash::make('BarcaM1d&01'),
 
                 'role' => 'admin',
 
